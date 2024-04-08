314811bfde9693f2cbd4cdb1781e91bc8aa9c8f5
tamani phiri <tamanigabriel0@gmail.com>
2024-04-08 19:58:11 +0200

    init

--- src/pages/PostListComponent.tsx
+++ src/pages/PostListComponent.tsx
@@ -3,12 +3,14 @@ import { c } from "../constants/c";
 const PostListComponent = () => {
   return (
     <section className=" min-h-screen w-full px-4 md:px-12">
-      {c.map((item, i) => (
-        <div key={i}>
-          <h1>{item.title}</h1>
-          <p>{item.description}</p>
-        </div>
-      ))}
+      <div className="">
+        {c.map((item, i) => (
+          <div key={i}>
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
