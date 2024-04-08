392899e0e3e570e82872e996f8c1f5c2b2802bd7
tamani phiri <tamanigabriel0@gmail.com>
2024-04-08 19:06:10 +0200

    c

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,14 @@
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <section className="min-h-screen w-full items-center justify-center flex">
+      <div className="flex flex-col border p-4">
+        <h1>Login</h1>
+        <input type="email" />
+        <input type="password" />
+        <button>Login</button>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
