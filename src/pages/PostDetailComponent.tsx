90782d5669bcc7c0dc3f5da32f87028a59940680
tamani phiri <tamanigabriel0@gmail.com>
2024-04-08 21:02:03 +0200

    init

--- src/pages/PostDetailComponent.tsx
+++ src/pages/PostDetailComponent.tsx
@@ -10,9 +10,14 @@ const PostDetailComponent: FC<Props> = ({ c }) => {
     <div className=" w-full flex flex-col">
       <div className="grid w-full grid-cols-1 items-center justify-center">
         {c.map((item, i) => (
-          <div key={i} className=" border-b p-4 border-gray-500">
-            <h1>{item.title}</h1>
-            <p>{item.description}</p>
+          <div key={i} className=" border-b p-4 border-gray-500 capitalize">
+            <h1>
+              Title: <span className=" text-gray-400">{item.title}</span>
+            </h1>
+            <p>
+              Description:{" "}
+              <span className=" text-gray-400">{item.description}</span>
+            </p>
           </div>
         ))}
       </div>
