fec58abf568ef1eb75e4f2d368a8ba793bbf7c6d
tamani phiri <tamanigabriel0@gmail.com>
2024-04-08 20:06:38 +0200

    init

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Toaster position="top-center" />
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <Toaster position="top-center" />
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/PostListComponent.tsx
+++ src/pages/PostListComponent.tsx
@@ -1,16 +1,27 @@
-import { c } from "../constants/c";
+import { useState } from "react";
+import { IPost } from "../interface/types";
+import PostDetailComponent from "./PostDetailComponent";
+import axios from "axios";
 
 const PostListComponent = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  const {} = useQuery(
+    "getPosts",
+    async () => {
+      const res = await axios.get("/");
+      return res.data;
+    },
+    {
+      onSuccess(data) {
+        console.log(data);
+      },
+    }
+  );
+
   return (
     <section className=" min-h-screen w-full px-4 md:px-12">
-      <div className="">
-        {c.map((item, i) => (
-          <div key={i}>
-            <h1>{item.title}</h1>
-            <p>{item.description}</p>
-          </div>
-        ))}
-      </div>
+      <PostDetailComponent c={posts} />
     </section>
   );
 };
