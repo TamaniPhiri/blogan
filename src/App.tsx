30ae8115de9b2ae20d0091fd191b51177cacbecd
tamani phiri <tamanigabriel0@gmail.com>
2024-04-08 19:40:52 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import PostListComponent from "./pages/PostListComponent";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={Login} />
+      <Route path="/home" Component={PostListComponent} />
     </Routes>
   );
 }
